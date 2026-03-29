310562d2d8ef3b25a0ceb353d14fdbb4e5e787ea
Noah Clarkson (agent) <mrnoahclarkson@gmail.com>
2026-03-29 19:52:10 +0000

    fix: gitignore .next build cache

--- client/.next/dev/static/chunks/src_app_page_tsx_02tnpw.._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_0~1i50r._.js","static/chunks/node_modules_next_dist_compiled_react_0f6hs59._.js"],
-    source: "dynamic"
-});
--- client/.next/server/chunks/_next-internal_server_app_api_generate-description_route_actions_0ou~a~6.js
+++
@@ -1,3 +0,0 @@
-module.exports=[14343,(e,o,d)=>{}];
-
-//# sourceMappingURL=_next-internal_server_app_api_generate-description_route_actions_0ou~a~6.js.map
